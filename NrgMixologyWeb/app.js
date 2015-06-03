78098ce2e43433be658bceee4ca486e46c76db0e
Jonathan Brown <jbrown@control4.com>
2015-06-03 12:32:38 -0600

    Initial Commit

---
+++ NrgMixologyWeb/app.js
@@ -0,0 +1,4 @@
+///<reference path="typings/angular2/angular2.d.ts"/>
+//import { boostrap, Component, View} from "angular2/angular2";
+//@Component({selector:"mixology"}) 
+//# sourceMappingURL=app.js.map
---
+++ NrgMixologyWeb/app.ts
@@ -0,0 +1,4 @@
+﻿///<reference path="typings/angular2/angular2.d.ts"/>
+
+//import { boostrap, Component, View} from "angular2/angular2";
+//@Component({selector:"mixology"})
