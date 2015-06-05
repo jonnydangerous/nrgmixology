4e2aa90b4bd922d44241c2914c0bd111e24ff8bd
Jonathan Brown <jbrown@control4.com>
2015-06-05 00:09:39 -0600

    Added Firebase support, wired up ratings

--- NrgMixologyWeb/app.ts
+++ NrgMixologyWeb/app.ts
@@ -1,9 +1,15 @@
 ﻿/// <reference path="scripts/typings/angularjs/angular.d.ts" />
 class MixologyApp {
+    module;
     constructor() {
         console.info("Starting App");
-        angular.module("MixologyApp", ['ngMaterial']);
+        this.module = angular.module("MixologyApp", ["ngMaterial", "MixologyApp.Core", "MixologyApp.Controllers","MixologyApp.Directives"]);
+    }
+
+    Constant(key, value) {
+        this.module.constant(key, value);
     }
 }
 
 var app = new MixologyApp();
+app.Constant("FIREBASE_URL", "https://nrgmixology.firebaseio.com/");
